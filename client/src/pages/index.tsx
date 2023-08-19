fd41d602b96da2ee3e8bc6ea989f832f2ee1bb06
aryan bramhane <aryanbramhane1@gmail.com>
2023-08-19 22:32:05 +0530

    feat: done with the landing page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,13 @@
 const Index = () => {
   return (
      <>
-     <h1>
-      Hello guyss
-     </h1>
+      <section className="" >
+        <div className="bg-[#FF595E] py-[328px] flex justify-center">
+         <h1 className=" text-8xl font-vt323 ">
+            Unravel the <span className="bg-[black] text-[#ff595e] block text-center w-72 mx-auto  ">Mystery</span> 
+         </h1>  
+        </div>
+      </section> 
      </> 
       )
 }
