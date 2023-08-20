a4009de5d0a9083a2072adf0d21d8d940bd647a1
aryan bramhane <aryanbramhane1@gmail.com>
2023-08-20 06:50:57 +0530

    feat: display of landing page is done

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,20 @@
+//import { Button } from "@radix-ui/themes"
+
 const Index = () => {
   return (
      <>
-      <section className="" >
-        <div className="bg-[#FF595E] py-[328px] flex justify-center">
+      <section className="h-full" >
+        <div className="bg-[#FF595E] py-[300px] w-full mx-auto text-center ">
          <h1 className=" text-8xl font-vt323 ">
-            Unravel the <span className="bg-[black] text-[#ff595e] block text-center w-72 mx-auto  ">Mystery</span> 
-         </h1>  
+            Unravel the <span className="bg-[black] text-[#ff595e] block text-center w-72 mx-auto ">Mystery</span> </h1>  
+            <button className = "font-vt323 text-2xl bg-[#FFCA3A] hover:bg-[#FFBC07] mt-4 px-6 py-2 rounded">Click Me</button> 
         </div>
       </section> 
+      <section>
+         
+      </section>
      </> 
+
       )
 }
 export default Index
