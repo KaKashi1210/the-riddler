a142c7d49f285e20bd99013b1fc0301ea48ee355
Atharva Pardeshi <atharva.exe@gmail.com>
2023-08-20 22:35:37 +0530

    style: format document (#3)

--- server/controllers/room.ts
+++ server/controllers/room.ts
@@ -6,40 +6,40 @@ const io = new Server()
 const roomRouter = express.Router()
 
 riddleRouter.get('/', () => {
-	io.on('connection', (socket) => {
+  io.on('connection', (socket) => {
+    console.log('Connected')
 
-		socket.on('login', ({ name, room }, callback) => {
-			try {
+    socket.on('login', ({ name, room }, callback) => {
+      try {
+        const user = addUser(socket.id, name, room)
+        if (!user) return callback(Error)
+        socket.join(user.room)
+        socket.in(room).emit('notification', { title: "Someone's here!", description: `${user.name} just entered the room.` })
+        io.in(room).emit('users', getUsers(room))
+        callback()
+      }
+      catch (error) {
+        console.log(error)
+      }
+    })
 
-			}
-			catch{
+    socket.on('sendMessage', (message) => {
+      const user = getUser(socket.id)
+      // send username and message
+      if (user) {
+        io.in(user.room).emit('message', { user: user.name, text: message });
+      }
+    })
 
-			}
-			const  user = addUser(socket.id, name, room)
-			if (!user) return callback(Error)
-			socket.join(user.room)
-			socket.in(room).emit('notification', { title: 'Someone\'s here', description: `${user.name} just entered the room` })
-			io.in(room).emit('users', getUsers(room))
-			callback()
-		})
+    socket.on('disconnect', () => {
+      console.log("User disconnected");
+      const user = deleteUser(socket.id)
+      if (user) {
+        io.in(user.room).emit('notification', { title: 'Someone just left', description: `${user.name} just left the room` })
+        io.in(user.room).emit('users', getUsers(user.room))
+      }
+    })
 
-		socket.on('sendMessage', (message) => {
-
-		const user = getUser(socket.id)
-			if(user){
-			io.in(user.room).emit('message', { user: user.name, text: message });
-			}
-		})
-
-		socket.on('disconnect', () => {
-			console.log("User disconnected");
-			const user = deleteUser(socket.id)
-			if (user) {
-				io.in(user.room).emit('notification', { title: 'Someone just left', description: `${user.name} just left the room` })
-				io.in(user.room).emit('users', getUsers(user.room))
-			}
-		})
-
-	})
+  })
 })
 export default roomRouter
--- server/utils/users.ts
+++ server/utils/users.ts
@@ -1,34 +1,34 @@
 import { User } from "../types"
 
-let users:User[] = []
+let users: User[] = []
 
-const addUser = (id:string, name:string, room:string): {  user :User } | { error : string } => {
+const addUser = (id: string, name: string, room: string): { user: User } | { error: string } => {
 
-    if (!name || !room) return { error: "Username or room must be provided" }
+  if (!name || !room) return { error: "Username or room must be provided" }
 
-    const existingUser = users.find(user => user.name.trim().toLowerCase() === name.trim().toLowerCase())
-    if (existingUser) return { error: "Username has already been taken" }
+  const existingUser = users.find(user => user.name.trim().toLowerCase() === name.trim().toLowerCase())
+  if (existingUser) return { error: "Username has already been taken" }
 
-    const user = { id, name, room }
-    users.push(user)
-    return { user }
+  const user = { id, name, room }
+  users.push(user)
+  return { user }
 }
 
 
-const getUser = (id:string): User|undefined => {
+const getUser = (id: string): User | undefined => {
 
-    let user = users.find(user=> user.id  == id)
+  let user = users.find(user => user.id == id)
 
-    return user
+  return user
 
 }
 
 
-const deleteUser = (id:User['id']) => {
-    const index = users.findIndex((user) => user.id === id);
-    if (index !== -1) return users.splice(index, 1)[0];
+const deleteUser = (id: User['id']) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) return users.splice(index, 1)[0];
 }
 
-const getUsers = (room:User['room']) => users.filter(user => user.room === room)
+const getUsers = (room: User['room']) => users.filter(user => user.room === room)
 
-export  { addUser , getUser, deleteUser, getUsers }
+export { addUser, getUser, deleteUser, getUsers }
